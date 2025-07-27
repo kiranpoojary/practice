5b1e0096b767b4417aa2eba2781fce94fb053f1f
Kiran Poojary <kiranpoojary483@gmail.com>
2025-07-27 18:52:49 +0530

    polyfill teaching

--- Js/Polyfills/13_flat.js
+++ Js/Polyfills/13_flat.js
@@ -1,19 +1,40 @@
-Array.prototype.myFlat = function (
-  maxLevel = 1,
-  currentLevel = 1,
-  result = []
-) {
-  for (const element of this) {
-    if (Array.isArray(element) && currentLevel <= maxLevel) {
-      element.myFlat(maxLevel, currentLevel + 1, result);
-    } else {
-      result.push(element);
+Array.prototype.myFlat = function (level = 1) {
+  //flat recursive
+  function flatMe(arr, maxLevel, currLevel = 1, result = []) {
+    for (const element of arr) {
+      if (Array.isArray(element) && currLevel <= maxLevel) {
+        flatMe(element, maxLevel, currLevel + 1, result);
+      } else {
+        result.push(element);
+      }
     }
+    return result;
   }
-  return result;
+
+  return flatMe(this, level);
 };
 
 let arr = [1, 2, [3, [4, [5]]]];
 console.log(arr.myFlat()); //[ 1, 2, 3, [ 4, [ 5 ] ] ]
 console.log(arr.myFlat(2)); //[ 1, 2, 3, 4, [ 5 ] ]
 console.log(arr.myFlat(Infinity)); //[ 1, 2, 3, 4, 5 ]
+
+// without recurssion
+// Array.prototype.myFlat = function (level = 1) {
+//   let result = [];
+//   let currArray = this;
+//   if (level == Infinity) level = 999;
+//   for (let currLevel = 0; currLevel < level; currLevel++) {
+//     result = [];
+//     for (let ele of currArray) {
+//       if (Array.isArray(ele)) {
+//         result = [...result, ...ele];
+//       } else {
+//         result.push(ele);
+//       }
+//       currArray = result;
+//     }
+//   }
+//   return result;
+// };
+// console.log(arr.myFlat(Infinity));
---
+++ test.js
@@ -0,0 +1,135 @@
+const myArray = [1, 3, 4, 6, 7, 9, 10];
+const myArrayObj = [
+  { name: "aa", age: 17 },
+  { name: "bb", age: 24 },
+  { name: "cc", age: 37 },
+];
+const nestedArray = [
+  1,
+  2,
+  [3, 4, [5, 6, [7, [7.4, 7.6]], 8], [9, 10, 11], 12],
+  13,
+];
+//
+//
+// map
+// console.log(myArray.map((value, index, arr) => value * 2));
+//
+// Array.prototype.myMap = function (cbFn) {
+//   const result = [];
+//   for (let i = 0; i < this.length; i++) {
+//     const ele = this[i];
+//     result.push(cbFn(ele, i, this));
+//   }
+//   return result;
+// };
+// console.log(myArray.myMap((e, i, arr) => e * 2));
+//
+//
+//
+//
+// filter
+//
+// console.log(myArray.filter((e, i, arr) => e % 2 == 0));
+//
+// Array.prototype.myFilter = function (cbFn) {
+//   const result = [];
+//   for (let i = 0; i < this.length; i++) {
+//     const ele = this[i];
+//     if (cbFn(ele, i, this)) result.push(ele);
+//   }
+//   return result;
+// };
+// console.log(myArray.myFilter((ele) => ele % 2 == 0));
+//
+// reduce
+//
+// console.log(
+//   myArray.reduce((res, cur, i, arr) => {
+//     console.log({ i, tt: arr });
+
+//     return res + cur;
+//   }, 0)
+// );
+//
+// Array.prototype.myReduce = function (cbFn, init) {
+//   let result = init;
+//   for (let i = 0; i < this.length; i++) {
+//     const ele = this[i];
+//     result = cbFn(result, ele, i, this);
+//   }
+//   return result;
+// };
+// console.log(
+//   myArray.myReduce((res, cur) => {
+//     res = res + cur;
+//     return res;
+//   }, 0)
+// );
+//
+// flat
+//
+// console.log(nestedArray.flat()); //default 1
+// console.log(nestedArray.flat(2));
+// console.log(nestedArray.flat(Infinity));
+//
+// Array.prototype.myFlat = function (level = 1) {
+//   let result = [];
+//   let currArray = this;
+//   if (level == Infinity) level = 999;
+//   for (let currLevel = 0; currLevel < level; currLevel++) {
+//     result = [];
+//     for (let ele of currArray) {
+//       if (Array.isArray(ele)) {
+//         result = [...result, ...ele];
+//       } else {
+//         result.push(ele);
+//       }
+//       currArray = result;
+//     }
+//   }
+//   return result;
+// };
+// console.log(nestedArray.myFlat(Infinity));
+//
+// once
+//
+//
+//
+//
+// memoization
+//
+//
+//
+//
+// debouncing
+//
+//
+//
+//
+// throttling
+//
+//
+//
+//
+// call
+//
+//
+//
+//
+// apply
+//
+//
+//
+//
+// bind
+//
+//
+//
+//
+// promise
+//
+//
+//
+//
+//
